26b9985dbf2e8f5b80c51efaeceaccca6c459114
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-15 19:45:28 -0300

    Change boxes color logic created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
+import { useState } from "react"
 import "./App.css"
 
+let order = 0
+
 function App() {
-	const getBoxes = () => {
+	const getBoxes = (type) => {
+		const boxesData = []
+
 		const boxes = [0, 1, 2].map((i) => {
 			return [0, 1, 2].map((j) => {
 				const key = `${i}-${j}`
 
 				if (!(i === 1 && j > 0)) {
-					return <div key={key} className="box"></div>
+					if (type === "initial")
+						return boxesData.push({ i, j, isClicked: false, order: null })
+
+					return (
+						<div
+							key={key}
+							className="box"
+							style={{
+								background:
+									boxState?.find((item) => item.i === i && item.j === j)
+										?.isClicked && "green",
+							}}
+							onClick={() => changeColor(i, j)}
+						></div>
+					)
 				}
 
 				return <div key={key}></div>
 			})
 		})
 
+		if (type === "initial") return boxesData
+
 		return boxes
+	}
+
+	const [boxState, setBoxState] = useState(getBoxes("initial"))
+
+	const changeColor = (i, j) => {
+		let temp = [...boxState]
+		const selectedBox = temp.find((item) => item.i === i && item.j === j)
+		selectedBox.isClicked = true
+		selectedBox.order = ++order
+		setBoxState(temp)
 	}
 
 	return (
