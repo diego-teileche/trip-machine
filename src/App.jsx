d0c267de774b430eb47ae969ee527c6ff76b354a
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-15 18:36:06 -0300

    Logic of the boxes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css"
 
 function App() {
+	const getBoxes = () => {
+		const boxes = [0, 1, 2].map((i) => {
+			return [0, 1, 2].map((j) => {
+				const key = `${i}-${j}`
+
+				if (!(i === 1 && j > 0)) {
+					return <div key={key} className="box"></div>
+				}
+
+				return <div key={key}></div>
+			})
+		})
+
+		return boxes
+	}
+
 	return (
-		<>
-			<h1>Diego!</h1>
-		</>
+		<div className="App">
+			<div className="box-container">{getBoxes()}</div>
+		</div>
 	)
 }
 
